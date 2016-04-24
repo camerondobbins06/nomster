90704c1257ed1c2e5ad8c1ed91411c511e019fa1
cameron <camerondobbins06@gmail.com>
2016-04-24 06:35:44 +0000

    Built a form to add new places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,8 +1,16 @@
 class PlacesController < ApplicationController
 
 	def index
-	@places = Place.all
-	end
+  @places=Place.paginate(:page => params[:page], :per_page => 5)
 end
+
+def new
+@place = Place.new
+end
+
+end
+
+
+
 
 
