1779bc80ed39392007be3de33934c98b87c1fb7a
cameron <camerondobbins06@gmail.com>
2016-05-15 21:28:26 +0000

    Moved the comment form into a modal

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
 
-  Ratings = {
+  RATINGS = {
   	'one star'		=> '1_star',
   	'two stars'		=> '2_stars',
   	'three stars'	=> '3_stars',
