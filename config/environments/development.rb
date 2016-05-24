6292b7d59724b03d54f7b49613134ca4be3d53db
cameron <camerondobbins06@gmail.com>
2016-05-24 02:34:08 +0000

    adjuste dthe automated mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,13 +1,13 @@
 Nomster::Application.configure do
-  config.action_mailer.delivery_method = :stmp
+  config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    address: "stmp.gmail.com",
+    address: "smtp.gmail.com",
     port: 587,
     domain: "example.com",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name ENV['GMAIL_ADDRESS'],
+    user_name: ENV['GMAIL_ADDRESS'],
     password: ENV['GMAIL_PASSWORD']
   }
   
